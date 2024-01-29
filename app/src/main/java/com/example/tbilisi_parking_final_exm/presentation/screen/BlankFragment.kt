25392850751c6481bcec0fe44fcd2abc6b81ddd4
Davitpurceladze <purceladzedavit@gmail.com>
2024-01-29 23:13:47 +0400

    create nav_graph for outlain

--- app/src/main/java/com/example/tbilisi_parking_final_exm/presentation/screen/BlankFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.tbilisi_parking_final_exm.presentation.screen
-
-import com.example.tbilisi_parking_final_exm.databinding.FragmentBlankBinding
-import com.example.tbilisi_parking_final_exm.presentation.base.BaseFragment
-
-
-class BlankFragment : BaseFragment<FragmentBlankBinding>(FragmentBlankBinding::inflate)
---
+++ app/src/main/java/com/example/tbilisi_parking_final_exm/presentation/screen/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.tbilisi_parking_final_exm.presentation.screen.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tbilisi_parking_final_exm.R
+import com.example.tbilisi_parking_final_exm.databinding.FragmentHomeBinding
+import com.example.tbilisi_parking_final_exm.presentation.base.BaseFragment
+
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+
+}
