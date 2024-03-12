4c312dc3d13e8708c3e610aacd0c9fb260779524
Lasha-Ilashvili <ilashvililasha@gmail.com>
2024-03-12 10:13:35 +0400

    added extension function and a class to add to apply formatting to edit text input

---
+++ app/src/main/java/com/example/tbilisi_parking_final_exm/presentation/extension/EditTextExtension.kt
@@ -0,0 +1,10 @@
+package com.example.tbilisi_parking_final_exm.presentation.extension
+
+import android.widget.EditText
+import com.example.tbilisi_parking_final_exm.presentation.screen.user_panel.wallet.adapter.CardInputAdapter
+
+
+fun EditText.applyFormatting(symbol: String, separator: Int) {
+    val cardInputAdapter = CardInputAdapter(editText = this, symbol = symbol, separator = separator)
+    addTextChangedListener(cardInputAdapter)
+}
--- app/src/main/java/com/example/tbilisi_parking_final_exm/presentation/screen/user_panel/wallet/WalletFragment.kt
+++ app/src/main/java/com/example/tbilisi_parking_final_exm/presentation/screen/user_panel/wallet/WalletFragment.kt
@@ -1,7 +1,9 @@
 package com.example.tbilisi_parking_final_exm.presentation.screen.user_panel.wallet
 
+import android.view.View.VISIBLE
 import com.example.tbilisi_parking_final_exm.databinding.FragmentWalletBinding
 import com.example.tbilisi_parking_final_exm.presentation.base.BaseFragment
+import com.example.tbilisi_parking_final_exm.presentation.extension.applyFormatting
 
 
 class WalletFragment : BaseFragment<FragmentWalletBinding>(FragmentWalletBinding::inflate) {
@@ -11,8 +13,16 @@ class WalletFragment : BaseFragment<FragmentWalletBinding>(FragmentWalletBinding
     }
 
     override fun bindViewActionListeners() {
+        with(binding) {
+            btnNext.setOnClickListener {
+                binding.cardDetailsLayout.root.visibility = VISIBLE
+            }
 
+            cardDetailsLayout.etCardNumberInput.applyFormatting(symbol = " ", separator = 4)
+            cardDetailsLayout.etCardDateInput.applyFormatting(symbol = "/", separator = 2)
+        }
     }
+
 
     override fun bindObserves() {
 
